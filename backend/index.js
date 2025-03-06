e9a9a7526c0e6ad97252c785392f5146d721847e
Amir Elbadaoui <amireb51@gmail.com>
2025-03-05 20:09:53 -0500

    Added update one book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -65,6 +65,30 @@ app.get('/books/:id', async (req, res) => {
     }
 });
 
+// Route for updating one book
+app.put('/books/:id', async (req, res) => {
+    try{
+        
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({message: 'Send all required fields: title, author, publishYear'});
+        }
+
+        const { id } = req.params;
+
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!result){
+            return res.status(404).json({message: 'Book not found'});
+        }
+
+        return res.status(200).json({message: 'Book updated successfully'});
+
+    } catch (err) {
+        console.log(error.message);
+        res.status(500).send({message : err.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(()=> {
