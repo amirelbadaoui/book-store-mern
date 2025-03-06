99793cbdb6a03dc3da71b67ad6fc9f725a9670d8
Amir Elbadaoui <amireb51@gmail.com>
2025-03-05 21:31:35 -0500

    Added cors & learned cors policy (l12-25:22)

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,24 @@ import {PORT, mongoDBURL} from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy
+// First Option: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Second Option: Allow Custom Origins ******* UPDATE Github Wiki with gap in knowledge
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (req, res) => {
     res.send("<h1>Wow</h1>");
