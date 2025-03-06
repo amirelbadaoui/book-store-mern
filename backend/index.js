c6356f7dc9a4ed5cae3a3308e69c0e9093409f04
Amir Elbadaoui <amireb51@gmail.com>
2025-03-05 20:28:20 -0500

    Added delete one book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -84,8 +84,26 @@ app.put('/books/:id', async (req, res) => {
         return res.status(200).json({message: 'Book updated successfully'});
 
     } catch (err) {
-        console.log(error.message);
+        console.log(err.message);
         res.status(500).send({message : err.message});
+    }
+});
+
+// Route for deleting one book by id
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return res.status(404).json({message: 'Book not found'});
+        }
+
+        return res.status(200).json({message: 'Book deleted successfully'});
+    } catch (err){
+        console.log(err.message);
+        return res.status(500).json({message: err.message});
     }
 });
 
