14e44daf9f15e95e016ebdf46324f16bcd792dd2
Amir Elbadaoui <amireb51@gmail.com>
2025-03-09 15:20:31 -0400

    Added showing books list in React

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,15 @@ app.use(express.json());
 
 // Middleware for handling CORS Policy
 // First Option: Allow All Origins with Default of cors(*)
-// app.use(cors());
+app.use(cors());
 // Second Option: Allow Custom Origins ******* UPDATE Github Wiki with gap in knowledge
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (req, res) => {
     res.send("<h1>Wow</h1>");
